53b4bb44ed9ec018e8bbd3622031e17bdc0e6d4f
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-09 00:50:02 -0400

    data layer done

--- db/index.js
+++ db/index.js
@@ -3,3 +3,62 @@ const { STRING, BOOLEAN } = Sequelize;
 
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_car_sales_db')
 
+const User = db.define('user',{
+    name: {
+        type: STRING
+    }
+})
+
+const Car = db.define('car', {
+    name: {
+        type: STRING
+    }
+})
+
+const Sale = db.define('sale', {
+    extendedWarenty:{
+        type: BOOLEAN,
+        defaultValue: true,
+    }
+})
+
+
+Sale.belongsTo(Car);
+Sale.belongsTo(User);
+
+
+const syncAndSeed = async () =>{
+    try{
+        await db.sync({ force : true });
+        console.log('connected to db')
+        const [moe, larry, lucy ] = await Promise.all(
+            ['moe', 'larry', 'lucy'].map( name =>{
+                return(User.create({ name: name }))
+            }))
+        
+        const [ford, toyta, audi] = await Promise.all(
+            ['ford', 'toyta', 'audi'].map( name =>{
+                return(Car.create({name : name}))
+            })
+        )
+
+        const sales = await Promise.all([
+        Sale.create({userId: moe.id, carId: ford.id}),
+        Sale.create({userId: moe.id, carId: audi.id})
+        ])
+        }
+    catch(err){
+        console.log(err)
+    }
+}
+
+
+module.exports = {
+    syncAndSeed,
+    db,
+    models: {
+        User,
+        Car,
+        Sale
+    }
+}
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+const {syncAndSeed, db, models :{ User, Car, Sale} } = require('./db/index')
+
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res, next)=>{
+    try{
+        res.send(`
+            <html>
+                <head>
+                </head>
+                <body>
+                    <h1>
+                        Hello World
+                    </h1>
+                </body>
+            </html>
+        `)
+    }
+    catch(err){
+        next(err)
+    }
+})
 
 
 const init = () =>{
     try{
         const port = process.env.PORT || 3000;
         app.listen(port, ()=> console.log(`listening on port ${port}`))
+        syncAndSeed();
     }
     catch(err){
         console.log(err)
