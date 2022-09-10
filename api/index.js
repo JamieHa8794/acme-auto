8d8c51c1491ae170ccde29d80b92bde647fa32d8
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-10 00:22:22 -0400

    refactor & use Router

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const { models :{ User, Car, Sale} } = require('../db/index')
+const router = require('express').Router();
+
+
+
+router.get('/users', async (req, res, next)=>{
+    try{
+        const Users = await User.findAll();
+        res.send(Users);
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.get('/cars', async (req, res, next)=>{
+    try{
+        const Cars = await Car.findAll();
+        res.send(Cars);
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.get('/sales', async (req, res, next)=>{
+    try{
+        const Sales = await Sale.findAll();
+        res.send(Sales);
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,39 +1,10 @@
 const {syncAndSeed, db, models :{ User, Car, Sale} } = require('./db/index')
+const router = require('./api/index');
 
 const express = require('express');
 const app = express();
 
-
-
-app.get('/api/users', async (req, res, next)=>{
-    try{
-        const Users = await User.findAll();
-        res.send(Users);
-    }
-    catch(err){
-        next(err)
-    }
-})
-
-app.get('/api/cars', async (req, res, next)=>{
-    try{
-        const Cars = await Car.findAll();
-        res.send(Cars);
-    }
-    catch(err){
-        next(err)
-    }
-})
-
-app.get('/api/sales', async (req, res, next)=>{
-    try{
-        const Sales = await Sale.findAll();
-        res.send(Sales);
-    }
-    catch(err){
-        next(err)
-    }
-})
+app.use('/api', require('./api'))
 
 
 
