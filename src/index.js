7e9ade8ac9899b41d2acec9dc12bb03b831b7345
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-11 00:10:23 -0400

    display user & car list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import axios from 'axios'
 
 
+const userList = document.querySelector('#user-list')
+const carList = document.querySelector('#car-list')
+const saleList = document.querySelector('#sale-list')
+
+
+
+const renderUsers = (users) =>{
+    const html = users.map(user =>{
+        return(`
+            <li>
+                <a href='#${user.id}'>
+                ${user.name}
+                </a>
+            </li>
+        `)
+    }).join('')
+    userList.innerHTML = html;
+}
+
+
+const renderCars = (cars) =>{
+    const html = cars.map(car =>{
+        return(`
+            <li>
+                <a href='#${car.id}'>
+                    ${car.name}
+                </a>
+            </li>
+        `)
+    }).join('');
+    carList.innerHTML = html;
+}
+
+
+
 const init = async () =>{
     try{
         const users = (await axios.get('/api/users')).data;
-        console.log(users)
         const cars = (await axios.get('/api/cars')).data;
-        console.log(cars)
+        console.log(users)
+
+        renderUsers(users);
+        renderCars(cars);
+
     }
     catch(err){
         console.log(err);
