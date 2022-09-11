5e9d525d80f978592358d9364fcf64d17391f758
JamieHa8794 <JamieHa8794@gmail.com>
2022-09-10 23:57:09 -0400

    fetch data using axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+import axios from 'axios'
+
+
+const init = async () =>{
+    try{
+        const users = (await axios.get('/api/users')).data;
+        console.log(users)
+        const cars = (await axios.get('/api/cars')).data;
+        console.log(cars)
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+init();
